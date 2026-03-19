90829618b76bcfc24bee9e0020ec5fb71da313f4
Nishtha Gupta <nishthasasaram555@gmail.com>
2026-03-19 17:32:23 +0530

    Day 2: learned functions, conditions, loops, arrays

--- basics.js
+++ basics.js
@@ -1,12 +1,2 @@
-console.log("Hello World")
-let myName = "Geetanjali"
-console.log(myName)
-const invoiceName = "InvoiceForge"
-let totalAmount = 100
-
-console.log(invoiceName)
-console.log(totalAmount)
-
-totalAmount = 200
-console.log(totalAmount)
-invoiceName = "Something Else"
+const items = [100, 200, 300, 400]
+console.log(items[2])
