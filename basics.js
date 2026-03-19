1f9fe48d8a57104d57c2527afb71d0315825e210
Nishtha Gupta <nishthasasaram555@gmail.com>
2026-03-19 17:20:48 +0530

    Day 1: learned variables, let vs const, console.log

---
+++ basics.js
@@ -0,0 +1,12 @@
+console.log("Hello World")
+let myName = "Geetanjali"
+console.log(myName)
+const invoiceName = "InvoiceForge"
+let totalAmount = 100
+
+console.log(invoiceName)
+console.log(totalAmount)
+
+totalAmount = 200
+console.log(totalAmount)
+invoiceName = "Something Else"
